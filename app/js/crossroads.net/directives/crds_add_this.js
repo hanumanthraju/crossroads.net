1116e092a98f371dea46a93b009fe9bf6570c11a
hanumanthraju <hanumanthraju.t@gmail.com>
2014-08-20 10:43:58 +0530

    MIL-477 handled code review comments

--- app/js/crossroads.net/directives/crds_add_this.js
+++ app/js/crossroads.net/directives/crds_add_this.js
@@ -3,11 +3,9 @@
 angular.module('crossroads')
 
 .directive('addThis', function() {
-    console.log('Loading addThis plugin directive');
     return {
         restrict: 'E',
         replace: true,
-        transclude: true,
         scope: {
             addThisTitle: '=addThisTitle',
             addThisUrl: '=addThisUrl'
--- app/js/crossroads.net/services/addthis.js
+++ app/js/crossroads.net/services/addthis.js
@@ -1,8 +1,5 @@
 'use strict';
 
-angular.module('crossroads');
-
 angular.element(document).ready(function() {
-    console.log('Initiate addThis plugin after dom ready');
     addthis.init();
 });
