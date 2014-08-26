9e5a6726fe9857f36c7f509fa83090fe232a8ad8
aravind666 <aravind.udayashankara@gmail.com>
2014-08-26 12:01:14 +0530

    Changed the service to load the publish ID on ready MIL-340

--- app/js/crossroads.net/services/addthis.js
+++ app/js/crossroads.net/services/addthis.js
@@ -1,5 +1,10 @@
 'use strict';
 
-angular.element(document).ready(function() {
-    addthis.init();
+angular.element(window).bind('load', function() {
+    var addThisScript = document.createElement('script');
+    addThisScript.setAttribute('type', 'text/javascript');
+    addThisScript.setAttribute('src', '//s7.addthis.com/js/300/addthis_widget.js#domready=1');
+    document.body.appendChild(addThisScript);
+    var addthis_config = addthis_config||{};
+    addthis_config.pubid = 'ra-5391d6a6145291c4';
 });
